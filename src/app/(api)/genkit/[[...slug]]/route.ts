dcc000c5fde7fd0c45bbe261206b6726fdf36c35
Aryan <engineer.aryan.work@gmail.com>
2025-09-07 05:06:01 +0000

    Attempted import error: 'nextHandler' is not exported from '@genkit-ai/n

--- src/app/(api)/genkit/[[...slug]]/route.ts
+++ src/app/(api)/genkit/[[...slug]]/route.ts
@@ -1,4 +1,4 @@
-import {nextHandler} from '@genkit-ai/next';
+import {nextHandler} from '@genkit-ai/next/server';
 import '@/ai/dev'; // Make sure your flows are loaded
 
 export const {GET, POST, PUT, DELETE, OPTIONS} = nextHandler();
